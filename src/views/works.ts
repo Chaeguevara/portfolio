25f1132ac582dae8aefdc92b971e89f242bb1ed3
Chaeguevara <dfso2@naver.com>
2025-05-03 20:47:44 +0900

    Update for deployment

--- src/views/works.ts
+++ src/views/works.ts
@@ -2,6 +2,7 @@ import { Works } from "../data/works";
 import { Card } from "../components/card";
 export function workView(subPath: number) {
   console.log("workview")
+  console.log(subPath)
   return `<h1> Welcome Works </h1>
 <p>This is the work page.</p>
 ${Object.entries(Works).map(([_, v]) => {
